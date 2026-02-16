c9193829685aeb08460c34ffd358578180df9ace
Ritika <ritikakumari87344@gmail.com>
2026-02-16 10:46:27 +0530

    indian time zone is updated

--- spindoproject/settings.py
+++ spindoproject/settings.py
@@ -137,13 +137,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-in'   # optional (Indian English)
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
